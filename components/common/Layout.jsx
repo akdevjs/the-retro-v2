b1451713352b861ea90a80d2b78726fd3816c076
Abdullah Khan <akdevjs@gmail.com>
2024-02-27 08:05:38 +0500

    Fixed the meta data and head of the website

--- components/common/Layout.jsx
+++ components/common/Layout.jsx
@@ -3,38 +3,37 @@ import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout = ({
-  children,
-  title = "The Retro",
-  description = "-description-",
-}) => (
+const Layout = ({ children }) => (
   //Render
   <>
     <Head>
-      <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link
-        rel="icon"
-        type="image/x-icon"
-        href="https://www.simba.kr/favicon.png"
-      ></link>
-
-      <meta name="description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content="https://www.simba.kr/logo.png" />
-      <meta property="og:image:type" content="image/png"></meta>
-      <meta property="og:url" content="https://www.simba.kr/" />
+      <link rel="icon" href="/favicon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta property="og:title" content="The Retro" />
+      <meta property="og:type" content="article" />
       <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        property="og:description"
+        content="You wanna get the orignal branded shoes on your door step? then you can use our platform. The retro provides the best branded shoes in the most affordable price on the internet."
       />
-      <script
-        defer
-        src="https://developers.kakao.com/sdk/js/kakao.min.js"
-      ></script>
+      <meta property="og:image" content="./imgs/Preview.jpg" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="description"
+        content="You wanna get the orignal branded shoes on your door step? then you can use our platform. The retro provides the best branded shoes in the most affordable price on the internet."
+      />
+      <link rel="apple-touch-icon" href="/logo192.png" />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+        crossOrigin="anonymous"
+      />
+
+      <link rel="manifest" href="/manifest.json" />
+
+      <title>The Retro - Best shoes selling site</title>
     </Head>
     <div>
       <Header />
